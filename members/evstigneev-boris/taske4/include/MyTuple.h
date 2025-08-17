c2a2ddad27ca823c27ca70ce58dc053c9ba62dcc
B0RISbl4 <BORISKAT110E4@YANDEX.RU>
2025-08-17 17:20:01 +0300

    Правки по MyTuple и тестам к нему

--- members/evstigneev-boris/taske4/include/MyTuple.h
+++ members/evstigneev-boris/taske4/include/MyTuple.h
@@ -1,28 +1,46 @@
 #pragma once
+#include <cstddef>
 #include <type_traits>
 
-template<typename T1, typename T2, typename T3>
-class MyTuple {
-private:
-    T1 v1;
-    T2 v2;
-    T3 v3;
+template<typename... Ts>
+class MyTuple;
 
+template<>
+class MyTuple<> {
 public:
-    MyTuple() = default;
-    MyTuple(const T1& a, const T2& b, const T3& c) : v1(a), v2(b), v3(c) {}
-
-    template <std::size_t I, typename = std::enable_if_t<(I < 3)>>
-    constexpr auto& get() {
-        if constexpr (I == 0) return v1;
-        else if constexpr (I == 1) return v2;
-        else return v3;
-    }
-
-    template <std::size_t I, typename = std::enable_if_t<(I < 3)>>
-    constexpr const auto& get() const {
-        if constexpr (I == 0) return v1;
-        else if constexpr (I == 1) return v2;
-        else return v3;
-    }
+    static constexpr size_t size() { return 0; }
 };
+
+template<typename Head, typename... Tail>
+class MyTuple<Head, Tail...> {
+public:
+    Head head;
+    MyTuple<Tail...> tail;
+
+    constexpr MyTuple(const Head& h, const Tail&... t) : head(h), tail(t...) {}
+
+    template <std::size_t I>
+    constexpr auto& get() {
+        static_assert(I < sizeof...(Tail) + 1, "Tuple index out of range");
+        if constexpr (I == 0) return head;
+        else return tail.template get<I - 1>();
+    }
+
+    template <std::size_t I>
+    constexpr const auto& get() const {
+        static_assert(I < sizeof...(Tail) + 1, "Tuple index out of range");
+        if constexpr (I == 0) return head;
+        else return tail.template get<I - 1>();
+    }
+
+    static constexpr size_t size() { return sizeof...(Tail) + 1; }
+};
+
+template <typename T, size_t I, typename = void>
+struct has_get_impl : std::false_type {};
+
+template <typename T, size_t I>
+struct has_get_impl<T, I, std::enable_if_t<(I < T::size())>> : std::true_type {};
+
+template <typename T, size_t I>
+constexpr bool has_get_v = has_get_impl<T, I>::value;
--- members/evstigneev-boris/taske4/tests/test_my_tuple.cpp
+++ members/evstigneev-boris/taske4/tests/test_my_tuple.cpp
@@ -1,88 +1,92 @@
 #include <gtest/gtest.h>
-#include <string>
 #include <type_traits>
 #include <utility>
 #include "../include/MyTuple.h"
 
-class MyTupleTest : public ::testing::Test {
-protected:
-    void SetUp() override {
-        intFloatStringTuple = MyTuple<int, float, std::string>(42, 3.14f, "hello");
-        stringBoolIntTuple = MyTuple<std::string, bool, int>("world", true, 100);
-        emptyTuple = MyTuple<int, float, double>();
-    }
-
-    MyTuple<int, float, std::string> intFloatStringTuple;
-    MyTuple<std::string, bool, int> stringBoolIntTuple;
-    MyTuple<int, float, double> emptyTuple;
-};
-
-template <typename T, size_t I, typename = void>
-struct has_get : std::false_type {};
-
-template <typename T, size_t I>
-struct has_get<T, I, std::void_t<decltype(std::declval<T>().template get<I>())>> 
-    : std::true_type {};
-
-template <typename T, size_t I>
-constexpr bool has_get_v = has_get<T, I>::value;
-
-TEST_F(MyTupleTest, ConstructionAndTypes) {
-    static_assert(std::is_same_v<decltype(intFloatStringTuple.get<0>()), int&>);
-    static_assert(std::is_same_v<decltype(intFloatStringTuple.get<1>()), float&>);
-    static_assert(std::is_same_v<decltype(intFloatStringTuple.get<2>()), std::string&>);
+TEST(MyTupleTest, ConstructionAndTypes) {
+    MyTuple<int, float, double> t(42, 3.14f, 2.718);
+    static_assert(std::is_same_v<decltype(t.get<0>()), int&>);
+    static_assert(std::is_same_v<decltype(t.get<1>()), float&>);
+    static_assert(std::is_same_v<decltype(t.get<2>()), double&>);
 }
 
-TEST_F(MyTupleTest, GetByIndex) {
-    EXPECT_EQ(intFloatStringTuple.get<0>(), 42);
-    EXPECT_FLOAT_EQ(intFloatStringTuple.get<1>(), 3.14f);
-    EXPECT_EQ(intFloatStringTuple.get<2>(), "hello");
-
-    const auto& constTuple = intFloatStringTuple;
-    EXPECT_EQ(constTuple.get<0>(), 42);
-    EXPECT_FLOAT_EQ(constTuple.get<1>(), 3.14f);
-    EXPECT_EQ(constTuple.get<2>(), "hello");
+TEST(MyTupleTest, GetByIndexAndConst) {
+    const MyTuple<int, float, double> t(42, 3.14f, 2.718);
+    EXPECT_EQ(t.get<0>(), 42);
+    EXPECT_FLOAT_EQ(t.get<1>(), 3.14f);
+    EXPECT_DOUBLE_EQ(t.get<2>(), 2.718);
 }
 
-TEST_F(MyTupleTest, ModificationThroughGet) {
-    intFloatStringTuple.get<0>() = 100;
-    EXPECT_EQ(intFloatStringTuple.get<0>(), 100);
-
-    stringBoolIntTuple.get<1>() = false;
-    EXPECT_EQ(stringBoolIntTuple.get<1>(), false);
+TEST(MyTupleTest, ModificationThroughGet) {
+    MyTuple<int, bool, double> t(1, true, 3.14);
+    t.get<0>() = 10;
+    t.get<1>() = false;
+    t.get<2>() = 2.718;
+    EXPECT_EQ(t.get<0>(), 10);
+    EXPECT_EQ(t.get<1>(), false);
+    EXPECT_DOUBLE_EQ(t.get<2>(), 2.718);
 }
 
-TEST_F(MyTupleTest, DefaultConstruction) {
-    EXPECT_EQ(emptyTuple.get<0>(), 0);
-    EXPECT_FLOAT_EQ(emptyTuple.get<1>(), 0.0f);
-    EXPECT_DOUBLE_EQ(emptyTuple.get<2>(), 0.0);
+TEST(MyTupleTest, EmptyTuple) {
+    MyTuple<> t;
+    static_assert(!has_get_v<decltype(t), 0>);
 }
 
-TEST_F(MyTupleTest, CompileTimeChecks) {
-    static_assert(has_get_v<decltype(intFloatStringTuple), 0>);
-    static_assert(has_get_v<decltype(intFloatStringTuple), 1>);
-    static_assert(has_get_v<decltype(intFloatStringTuple), 2>);
-
-    static_assert(!has_get_v<decltype(intFloatStringTuple), 3>);
-    static_assert(!has_get_v<decltype(intFloatStringTuple), static_cast<size_t>(-1)>);
+TEST(MyTupleTest, SingleElementTuple) {
+    MyTuple<int> t(42);
+    EXPECT_EQ(t.get<0>(), 42);
+    t.get<0>() = 99;
+    EXPECT_EQ(t.get<0>(), 99);
+    static_assert(has_get_v<decltype(t), 0>);
+    static_assert(!has_get_v<decltype(t), 1>);
 }
 
-TEST_F(MyTupleTest, VariousTypeCombinations) {
-    MyTuple<std::string, std::string, std::string> stringTuple("a", "b", "c");
-    EXPECT_EQ(stringTuple.get<0>(), "a");
-    EXPECT_EQ(stringTuple.get<1>(), "b");
-    EXPECT_EQ(stringTuple.get<2>(), "c");
+TEST(MyTupleTest, TwoElementsTuple) {
+    MyTuple<int, bool> t(7, true);
+    EXPECT_EQ(t.get<0>(), 7);
+    EXPECT_EQ(t.get<1>(), true);
+    t.get<0>() = 55;
+    t.get<1>() = false;
+    EXPECT_EQ(t.get<0>(), 55);
+    EXPECT_EQ(t.get<1>(), false);
+}
 
+TEST(MyTupleTest, ManyElementsTuple) {
+    MyTuple<int, float, char, bool, double> t(1, 2.5f, 'x', true, 9.99);
+    EXPECT_EQ(t.get<0>(), 1);
+    EXPECT_FLOAT_EQ(t.get<1>(), 2.5f);
+    EXPECT_EQ(t.get<2>(), 'x');
+    EXPECT_EQ(t.get<3>(), true);
+    EXPECT_DOUBLE_EQ(t.get<4>(), 9.99);
+
+    t.get<0>() = 10;
+    t.get<2>() = 'z';
+    EXPECT_EQ(t.get<0>(), 10);
+    EXPECT_EQ(t.get<2>(), 'z');
+
+    static_assert(has_get_v<decltype(t), 0>);
+    static_assert(has_get_v<decltype(t), 4>);
+    static_assert(!has_get_v<decltype(t), 5>);
+}
+
+TEST(MyTupleTest, InvalidIndices) {
+    using Tuple3 = MyTuple<int, float, double>;
+
+    static_assert(!has_get_v<Tuple3, 3>);
+    static_assert(!has_get_v<Tuple3, static_cast<size_t>(-1)>);
+}
+
+TEST(MyTupleTest, StructElements) {
     struct Point { int x, y; };
-    MyTuple<Point, Point, Point> structTuple(Point{1,2}, Point{3,4}, Point{5,6});
-    EXPECT_EQ(structTuple.get<0>().x, 1);
-    EXPECT_EQ(structTuple.get<1>().y, 4);
-    EXPECT_EQ(structTuple.get<2>().x, 5);
+    MyTuple<Point, Point, Point> t(Point{1,2}, Point{3,4}, Point{5,6});
+    EXPECT_EQ(t.get<0>().x, 1);
+    EXPECT_EQ(t.get<1>().y, 4);
+    EXPECT_EQ(t.get<2>().x, 5);
 }
 
-TEST_F(MyTupleTest, ConstCorrectness) {
-    const auto& constTuple = intFloatStringTuple;
-    static_assert(std::is_same_v<decltype(constTuple.get<0>()), const int&>);
-    static_assert(std::is_same_v<decltype(constTuple.get<1>()), const float&>);
-    static_assert(std::is_same_v<decltype(constTuple.get<2>()), const std::string&>);
+TEST(MyTupleTest, ConstCorrectness) {
+    const MyTuple<int, float, double> t(1, 2.5f, 3.14);
+    static_assert(std::is_same_v<decltype(t.get<0>()), const int&>);
+    static_assert(std::is_same_v<decltype(t.get<1>()), const float&>);
+    static_assert(std::is_same_v<decltype(t.get<2>()), const double&>);
 }
